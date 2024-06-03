b2aaa1dce94968891e943691e766b5c86a2be699
jonemilnik <jonathanerivera2501@gmail.com>
2024-06-04 00:52:34 +0100

    Add Simple.css and Libre Baskerville global font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,12 +12,32 @@ const articlePaths = [
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', "@nuxtjs/google-fonts"],
   ssr: true,
   hooks: {
     async "nitro:config" (nitroConfig) {
       if (nitroConfig.dev) { return }
       nitroConfig.prerender?.routes?.push(...articlePaths);
     },
-  }
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap",
+        },
+      ],
+    },
+  },
+  css: ['/assets/main.css']
 })
